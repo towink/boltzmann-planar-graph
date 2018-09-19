645b22d232b8de4c8c38fd5aa255d23383b14e4e
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2018-09-19 15:19:56 +0200

    First version of planar_graph_generator, the file that provides the user interface.

--- planar_graph_sampler/planar_graph_generator.py
+++ planar_graph_sampler/planar_graph_generator.py
@@ -11,235 +11,125 @@
 #           Petre Petrov <petrepp4@gmail.com>
 #           Rudi Floren <rudi.floren@gmail.com>
 #           Tobias Winkler <tobias.winkler1@rwth-aachen.de>
-from numpy import diff
 
-from planar_graph_sampler.grammar.planar_graph_decomposition import  planar_graph_grammar, bij_connected_comps
+import math
+import multiprocessing as multiproc
+
+from planar_graph_sampler.grammar.planar_graph_decomposition import planar_graph_grammar, comps_to_planar_embedding
 from framework.evaluation_oracle import EvaluationOracle
 from framework.generic_samplers import BoltzmannSamplerBase
 from framework.generic_classes import SetClass
 from planar_graph_sampler.evaluations_planar_graph import planar_graph_evals
 import networkx as nx
 import datetime
-import multiprocessing as mp
-
-class PlanarGraphGenerator:
-
-    def generate_planar_graph(self, node_number, variance):
-        """This function generates a random planar graph using Boltzmann
-        samplers with fixed number of nodes with the possibility to
-        define a margin in which the number of nodes of the sampled
-        planar graph can vary.
-
-        Parameters
-        ----------
-        node_number: int
-            the desired number of nodes the graph should have
-        variance: float
-            the variance for the number of nodes in percent
-
-        Returns
-        -------
-        graph: NetworkX
-            a random networkx planar graph
-
-        Notes
-        -----
-        This algorithm makes use of the fact that every planar graph can
-        be decomposed into a set of connected planar graphs which in
-        turn can be obtained by using several bijection and rejection
-        steps starting with bicolored binary trees. For every step one
-        creates a Boltzmann sampler that produces a certain random
-        object with uniform probability. Combining all the samplers,
-        one obtain a Boltzmann sampler for planar graphs [1].
-
-        References
-        ----------
-        .. [1] Eric Fusy:
-            Uniform random sampling of planar graphs in linear time
-        """
-        # If the user specifies a variance of 100, then there are no
-        #  lower/upper bounds
-        if variance != 100:
-            lower_bound = node_number - (node_number * variance/100)
-            upper_bound = node_number + (node_number * variance/100)
-
-        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)
 
 
-        grammar = planar_graph_grammar()
-        grammar.init()
-        grammar.precompute_evals('G_dx_dx', 'x', 'y')
-        curr_node_number = 0
-        
-        planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-
-        if variance != 100:
-            curr_node_number = planar_graph.l_size
-            while(curr_node_number < lower_bound or curr_node_number > upper_bound):
-                planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-                curr_node_number = planar_graph.l_size
-
-        # Transform to networkx graph
-        gnx = bij_connected_comps(planar_graph)
-        return gnx
+def random_planar_graph(n, epsilon=0.1, require_connected=True,
+                        with_embedding=True, allow_multiproc=False):
+    """See PlanarGraphGenerator."""
+    return PlanarGraphGenerator(n, epsilon, require_connected,
+                                with_embedding, allow_multiproc).sample()
 
 
-    def generate_planar_graph_with_statistics(self, node_number, variance, oracle = None):
-        """This function generates a random planar graph using Boltzmann
-        samplers with fixed number of nodes with the possibility to
-        define a margin in which the number of nodes of the sampled
-        planar graph can vary.
+class PlanarGraphGenerator(object):
+    """
+    Sets up a uniform random generator for planar graphs.
 
-        Parameters
-        ----------
-        node_number: int
-            the desired number of nodes the graph should have
-        variance: float
-            the variance for the number of nodes in percent
+    Parameters
+    ----------
+    n : int
+        Number of nodes, must be at least 3
+    epsilon : float, optional (default=0.1)
+        Enables approximate size sampling of graphs with number of nodes
+        in the interval [n(1-eps), n(1+eps)]. Set to a value smaller than 1/n for exact size sampling.
+    require_connected : bool, optional (default=False)
+        Sample from the class of connected planar graphs instead of general planar graphs.
+    with_embedding : bool, optional (default=True)
+        If set to True, the generated graphs are returned with an arbitrary planar embedding represented as
+        an nx.PlanarEmbedding object. Otherwise an nx.Graph object is returned.
+    allow_multiproc : bool, optional (default=False)
+        Allows usage of the multiprocessing module for parallel sampling.
 
-        Returns
-        -------
-        graph: NetworkX
-            a random networkx planar graph
+    Returns
+    -------
+    G : Graph
+        Planar graph drawn uniformly at random. The nodes are labeled with consecutive integers
+        starting from 1. If `with_embedding` is set to ``True``, an ``nx.PlanarEmbedding`` object is returned,
+        otherwise ``nx.Graph``.
 
-        Notes
-        -----
-        This algorithm makes use of the fact that every planar graph can
-        be decomposed into a set of connected planar graphs which in
-        turn can be obtained by using several bijection and rejection
-        steps starting with bicolored binary trees. For every step one
-        creates a Boltzmann sampler that produces a certain random
-        object with uniform probability. Combining all the samplers,
-        one obtain a Boltzmann sampler for planar graphs [1].
+    Notes
+    -----
+    Expected running time is O(n/eps).
+    In particular, the expected time for exact size sampling is O(n²).
 
-        References
-        ----------
-        .. [1] Eric Fusy:
-            Uniform random sampling of planar graphs in linear time
-        """
-        # If the user specifies a variance of 100, then there are no
-        #  lower/upper bounds
-        if variance != 100:
-            lower_bound = node_number - (node_number * variance / 100)
-            upper_bound = node_number + (node_number * variance / 100)
+    References
+    ----------
+    .. [1] E. Fusy:
+        Uniform random sampling of planar graphs in linear time
+    """
 
-        BoltzmannSamplerBase.oracle = oracle
-        if oracle is None:
-            BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)
+    def __init__(self, n, epsilon=0.1, require_connected=True,
+                 with_embedding=True, allow_multiproc=False):
+        # Handle invalid arguments.
+        if n < 3:
+            raise ValueError("n must be an integer greater or equal than 3")
+        self._n = n
+        if epsilon > 1 or epsilon < 0:
+            raise ValueError("epsilon must be a real number in [0,1]")
+        self._eps = epsilon
 
-        grammar = planar_graph_grammar()
-        grammar.init()
-        grammar.precompute_evals('G_dx_dx', 'x', 'y')
-        curr_node_number = 0
+        # Compute interval of accepted sizes.
+        self._lower = math.ceil(n * (1 - epsilon))
+        self._upper = math.floor(n * (1 + epsilon))
 
-        planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
+        self._require_connected = require_connected
+        self._with_embedding = with_embedding
+        self._allow_parallel = allow_multiproc
+        if allow_multiproc:
+            self.sample = self._sample_multiproc
+        else:
+            self.sample = self._sample_single_proc
 
-        lower_bound_restriction_error_count = 0
-        upper_bound_restricctio_error_count = 0
-        if variance != 100:
-            curr_node_number = planar_graph.l_size
-            while (curr_node_number < lower_bound or curr_node_number > upper_bound):
-                if curr_node_number < lower_bound:
-                    lower_bound_restriction_error_count += 1
+        # Set up the oracle and grammar for sampling.
+        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(n, planar_graph_evals)
+        self._grammar = planar_graph_grammar()
+        self._grammar.init()
+        self._grammar.precompute_evals('G_dx_dx_dx', 'x', 'y')
+
+    def sample(self):
+        """Invokes the random generator once."""
+        # This method is set in __init__.
+        pass
+
+    def _sample_single_proc(self):
+        if self._require_connected:
+            sampled_class = 'G_1_dx_dx_dx'
+        else:
+            sampled_class = 'G_dx_dx_dx'
+        while True:
+            half_edge_graph = self._grammar.sample_iterative(sampled_class).underive_all()
+            if self._lower <= half_edge_graph.number_of_nodes() <= self._upper:
+                if self._with_embedding:
+                    # TODO Relabel the nodes.
+                    return half_edge_graph.to_planar_embedding()
                 else:
-                    upper_bound_restricctio_error_count += 1
+                    return half_edge_graph.to_networkx_graph(relabel=True)
 
-                if lower_bound_restriction_error_count % 1000 == 0:
-                    print(lower_bound_restriction_error_count)
+    def _sample_multiproc(self):
+        cpu_count = multiproc.cpu_count()
+        processes_queue = multiproc.Queue()
 
-                planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-                curr_node_number = planar_graph.l_size
-
-
-        print("Lower bound restruction errors: %s " % lower_bound_restriction_error_count)
-        print("Upper bound restruction errors: %s " % upper_bound_restricctio_error_count)
-
-        # Transform to networkx graph
-        gnx = bij_connected_comps(planar_graph)
-        return gnx, lower_bound_restriction_error_count, upper_bound_restricctio_error_count
-
-    def multiprocessing_target_function(self, queue, N, variance):
-        result = self.generate_planar_graph_with_statistics(N, variance)
-        queue.put(result)
-
-    def initial_multiprocess_implementation(self, N, variance):
-
-        number_of_cpus = mp.cpu_count()
-        processes_queue = mp.Queue()
-        processes = [mp.Process(
-            target=self.multiprocessing_target_function,
-            args=(processes_queue, N, variance, )) for i in range(number_of_cpus)]
+        processes = [multiproc.Process(
+            target=self._sample_single_proc,
+            args=self) for _ in range(cpu_count)]
 
         for p in processes:
             p.daemon = True
             p.start()
 
-        # Wait for the first result
+        # Wait for the first result.
         result = processes_queue.get()
-        # Terminate the other processes
+        # Terminate the other processes and return result.
         for p in processes:
             p.terminate()
-        # Retrun the result
         return result
-
-
-    def generate_planar_graph_fixed_nodes_edges(self, node_num, edge_num, node_var, edge_var):
-        """This function samples random planar graphs using Boltzmann
-        samplers with fixed number of nodes and edges with the
-        possibility of choosing a variance for number of nodes and edges.
-        """
-        if node_var != 100:
-            lower_nodes = node_num - (node_num * node_var)
-            upper_nodes = node_num + (node_num * node_var)
-        if edge_var != 100:
-            lower_edges = edge_num - (edge_num * edge_var)
-            upper_edges = edge_num + (edge_num * edge_var)
-
-        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)  
-
-        grammar = planar_graph_grammar()
-        grammar.init()
-        grammar.precompute_evals('G_dx_dx', 'x', 'y')
-        curr_node_number = 0
-        curr_edge_number = 0
-
-        if node_var != 100 and edge_var != 100:
-            while(curr_node_number < lower_nodes or curr_node_number > upper_nodes
-                    or curr_edge_number < lower_edges or curr_edge_number > upper_edges):
-                planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-                curr_node_number = planar_graph.l_size
-                curr_edge_number = planar_graph.u_size
-        elif node_num != 100 and edge_var == 100:
-            while(curr_node_number < lower_nodes or curr_node_number > upper_nodes):
-                planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-                curr_node_number = planar_graph.l_size
-        elif node_num == 100 and edge_var != 100:
-            while(curr_node_number < lower_nodes or curr_node_number > upper_nodes):
-                planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-                curr_node_number = planar_graph.l_size
-        else:
-            planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-
-        # Transform to networkx graph
-        gnx = bij_connected_comps(planar_graph)
-        return gnx
-
-    def generate_planar_graph_with_min_number_nodes(self, node_number):
-        """This function samples random planar graphs using Boltzmann samplers
-        with at least the given number of desired nodes.
-        """
-
-        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)  
-
-        grammar = planar_graph_grammar()
-        grammar.init()
-        grammar.precompute_evals('G_dx_dx', 'x', 'y')
-        curr_node_number = 0
-
-        while(curr_node_number < node_number):
-            planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-            curr_node_number = planar_graph.l_size
-
-        # Transform to networkx graph
-        gnx = bij_connected_comps(planar_graph)
-        return gnx
